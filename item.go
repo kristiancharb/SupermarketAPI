399042d12267cf7d45d3ac2b4939b8b1840ac93f
Kristian Charbonneau <kristiancharb@gmail.com>
2021-03-26 13:17:40 -0400

    Add item code validation logic

--- item.go
+++ item.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 )
 
 type Item struct {
@@ -15,6 +16,21 @@ type ItemList struct {
 	Items []Item `json:"items"`
 }
 
+type BadCodeError struct {
+	Code string
+	Err  error
+}
+
+type ItemNotFoundError struct {
+	Code string
+	Err  error
+}
+
+type ItemConflictError struct {
+	Code string
+	Err  error
+}
+
 func (item *Item) Bind(r *http.Request) error {
 	if item.Name == "" {
 		return fmt.Errorf("name is a required field")
@@ -25,6 +41,9 @@ func (item *Item) Bind(r *http.Request) error {
 	if item.Price == 0 {
 		return fmt.Errorf("price is a required field")
 	}
+	if !isValidCode(item.Code) {
+		return fmt.Errorf("code is invalid")
+	}
 	return nil
 }
 
@@ -34,8 +53,8 @@ func (*Item) Render(w http.ResponseWriter, r *http.Request) error {
 
 func (item *Item) equals(other *Item) bool {
 	return (item.Name == other.Name &&
-			item.Code == other.Code &&
-			item.Price == other.Price)
+		item.Code == other.Code &&
+		item.Price == other.Price)
 }
 
 func (*ItemList) Render(w http.ResponseWriter, r *http.Request) error {
@@ -52,5 +71,21 @@ func (list *ItemList) Bind(r *http.Request) error {
 	return nil
 }
 
+func isValidCode(code string) bool {
+	codePattern := "^(?:[A-Za-z0-9]{4}-){3}[A-Za-z0-9]{4}$"
+	matched, _ := regexp.MatchString(codePattern, code)
+	return matched
+}
 
+func (e *BadCodeError) Error() string {
+	return e.Code + ": " + e.Err.Error()
+}
+
+func (e *ItemNotFoundError) Error() string {
+	return e.Code + ": " + e.Err.Error()
+}
+
+func (e *ItemConflictError) Error() string {
+	return e.Code + ": " + e.Err.Error()
+}
 
--- supermarket_test.go
+++ supermarket_test.go
@@ -34,11 +34,28 @@ func TestAddItems(t *testing.T) {
 	compareItemsLists(t, expected, fetch())
 }
 
-func TestDeleteItems(t *testing.T) {
+func TestDeleteItem(t *testing.T) {
 	initDb()
 	expected := getInitialTestItems()
 	deleteItem("A12T-4GH7-QPL9-3N4M")
 	compareItemsLists(t, expected[1:], fetch())
+}
+
+func TestIsValid(t *testing.T) {
+	validCodes := []string{
+		"A12T-4GH7-QPL9-3N4M",
+		"A12t-4Gh7-QPl9-3N4m",
+		"1111-1111-1111-1111",
+		"HHHH-HHHH-HHHH-HHHH",
+	}
+	invalidCodes := []string{
+		"A12-4GH7-QPL9-3N4M",
+		"A12T4GH7-QPL9-3N4M",
+		"A12T-4GH7-QPL9-3N4MS",
+		"A12?-4GH7-QPL9-3N4MS",
+	}
+	checkCodes(t, validCodes, true)
+	checkCodes(t, invalidCodes, false)
 }
 
 func compareItemsLists(t *testing.T, expected, actual []Item) {
@@ -51,6 +68,14 @@ func compareItemsLists(t *testing.T, expected, actual []Item) {
 	for i := range expected {
 		if !expected[i].equals(&actual[i]) {
 			t.Fatalf(`Expected: %+v Actual: %+v`, expected[i], actual[i])
+		}
+	}
+}
+
+func checkCodes(t *testing.T, codes []string, expected bool) {
+	for _, code := range codes {
+		if isValid := isValidCode(code); isValid != expected {
+			t.Fatalf(`%s -> Expected: %t Actual: %t`, code, expected, isValid)
 		}
 	}
 }
